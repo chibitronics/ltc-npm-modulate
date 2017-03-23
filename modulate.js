77e1a687205e22a5b9e5bb86dbcf107e96ccfe0e
Sean Cross <sean@xobs.io>
2017-03-23 12:32:31 +0800

    modulate: remove modulateStream and reformat file
    Signed-off-by: Sean Cross <sean@xobs.io>

--- modulate.js
+++ modulate.js
@@ -1,6 +1,6 @@
 var FskEncoder = require("./afsk-encoder.js");
 
-var Modulator = function(params) {
+var Modulator = function (params) {
 
     if (!params)
         params = new Object();
@@ -24,50 +24,7 @@ Modulator.prototype = {
     scriptNode: null, // Re-used script node object for audio generation
     can_stop: true, // Whether we can stop (usually we can)
 
-    // modulate a single packet. The data to modulate should be Uint8 format
-    // This function allocates an audio buffer based on the length of the data and the sample rate
-    // It then calls the fsk modulator, and shoves the returned floating point array
-    // into the audio context for later playback
-    modulateStream: function(data) {
-
-        // this odd construct is for safari compatibility
-        if (!("audioContext" in window))
-            window.audioContext = new(window.AudioContext || window.webkitAudioContext)();
-
-        if (!"rate" in this)
-            this.rate = window.audioContext.sampleRate;
-
-        if (!this.script_node) {
-            Modulator.prototype.script_node = window.audioContext.createScriptProcessor(4096, 2, 2);
-
-            // If the script node has an invalid buffer size, force one with a nonzero buffer.
-            if (!Modulator.prototype.script_node.bufferSize) {
-
-                // IE with a polyfill exhibits this problem, and crashes when you try to stop.
-                Modulator.prototype.script_node = window.audioContext.createScriptProcessor(4096, 2, 2);
-                this.prototype.can_stop = false;
-            }
-            this.script_node = Modulator.prototype.script_node;
-        }
-
-        var bufLen = Math.ceil(data.length * 8 * this.encoder.samplesPerBit());
-        this.outputAudioBuffer = window.audioContext.createBuffer(1, bufLen, this.rate);
-
-        var timeStart = performance.now();
-
-        var outputFloatArray = this.outputAudioBuffer.getChannelData(0);
-        this.encoder.modulate(data, outputFloatArray); // writes outputFloatArray in-place
-
-        // How far into the outputAudioBuffer we are.
-        this.script_node_offset = 0;
-
-        var timeEnd = performance.now();
-        var timeElapsed = timeEnd - timeStart;
-        console.log("Rendered " + data.length + " data bytes in " +
-            timeElapsed.toFixed(2) + "ms");
-    },
-
-    silence: function(msecs) {
+    silence: function (msecs) {
         var bufLen = Math.ceil(window.audioContext.sampleRate / (1000.0 / msecs));
         this.outputAudioBuffer = window.audioContext.createBuffer(1, bufLen, window.audioContext.sampleRate);
         var outputFloatArray = this.outputAudioBuffer.getChannelData(0);
@@ -76,7 +33,7 @@ Modulator.prototype = {
         this.script_node_offset = 0;
     },
 
-    processAudio: function(ev) {
+    processAudio: function (ev) {
         var outl = ev.outputBuffer.getChannelData(0);
         var outr = ev.outputBuffer.getChannelData(1);
 
@@ -112,12 +69,12 @@ Modulator.prototype = {
     },
 
     // immediately play the modulated audio exactly once. Useful for debugging single packets
-    playBuffer: function(obj, func) {
+    playBuffer: function (obj, func) {
         console.log("-- playAudioBuffer --");
         var bufferNode = window.audioContext.createBufferSource();
         bufferNode.buffer = this.outputAudioBuffer;
         bufferNode.connect(window.audioContext.destination); // Connect to speakers
-        bufferNode.addEventListener("ended", function() {
+        bufferNode.addEventListener("ended", function () {
             var playTimeEnd = performance.now();
             var timeElapsed = playTimeEnd - this.playTimeStart;
             console.log("got audio ended event after " + timeElapsed.toFixed(2) + "ms");
@@ -133,9 +90,9 @@ Modulator.prototype = {
     // tells where to start playing. You could, in theory, start modulating
     // part-way through an audio stream by setting index to a higher number on your
     // first call.
-    playLoop: function(obj, end_func, param) {
+    playLoop: function (obj, end_func, param) {
 
-        this.get_more_data = function() {
+        this.get_more_data = function () {
             if (!end_func.call(obj, param)) {
                 this.playing = false;
                 return false;
@@ -143,7 +100,7 @@ Modulator.prototype = {
             return true;
         };
 
-        this.script_node.onaudioprocess = function(ev) {
+        this.script_node.onaudioprocess = function (ev) {
             Modulator.prototype.processAudio.call(this, ev);
         }.bind(this);
 
@@ -153,7 +110,7 @@ Modulator.prototype = {
         }
     },
 
-    modulatePcm: function(data, type) {
+    modulatePcm: function (data, type) {
 
         var bufLen = Math.ceil(data.length * 8 * this.encoder.samplesPerBit());
         var modulatedData = new Float32Array(bufLen);
@@ -198,6 +155,6 @@ Modulator.prototype = {
 }
 
 // AMD exports
-if (typeof module !== "undefined"  && module.exports) {
-  module.exports = Modulator;
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = Modulator;
 }
