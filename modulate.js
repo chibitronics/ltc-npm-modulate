ae86072149134cf4f7cd6f87d1ecfc14894d67b6
Sean Cross <sean@xobs.io>
2017-07-14 16:40:01 +0800

    modulate: remove dead realtime modulation code
    The code used to be realtime, but this proved to be unreliable on older
    devices.  As such, we've moved to an ahead-of-time method.
    
    This code is ancient, and hasn't been used in a long time.  Remove it.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- modulate.js
+++ modulate.js
@@ -28,93 +28,12 @@ Modulator.prototype = {
     scriptNode: null, // Re-used script node object for audio generation
     can_stop: true, // Whether we can stop (usually we can)
 
-    silence: function(msecs) {
-        var bufLen = Math.ceil(window.audioContext.sampleRate / (1000.0 / msecs));
-        this.outputAudioBuffer = window.audioContext.createBuffer(1, bufLen, window.audioContext.sampleRate);
-        var outputFloatArray = this.outputAudioBuffer.getChannelData(0);
-        for (var i = 0; i < outputFloatArray.length; i++)
-            outputFloatArray[i] = 0;
-        this.script_node_offset = 0;
-    },
-
-    processAudio: function(ev) {
-        var outl = ev.outputBuffer.getChannelData(0);
-        var outr = ev.outputBuffer.getChannelData(1);
-
-        // If we're not playing, but still being called, just fill the channel with silence.
-        if (!this.playing) {
-            for (var i = 0; i < outl.length; i++)
-                outl[i] = outr[i] = 0;
-
-            // Some browsers crash when you stop playing
-            if (this.can_stop)
-                this.script_node.disconnect();
-            return;
+    modulatePcm: function(data, type, useLBR) {
+        // Re-create the encoder if LBR mode has changed.
+        if ((useLBR !== undefined) && (this.lbr !== useLBR)) {
+            this.lbr = useLBR;
+            this.encoder = new FskEncoder(this.rate, this.lbr);
         }
-
-        var outputFloatArray = this.outputAudioBuffer.getChannelData(0);
-
-        for (var i = 0; i < outl.length; i++) {
-            if (this.script_node_offset >= outputFloatArray.length) {
-                // If there's more data to play, reset the output float array.
-                if (this.get_more_data())
-                    outputFloatArray = this.outputAudioBuffer.getChannelData(0);
-
-                // Otherwise, fill the buffer with 0s, and we'll stop playing on the next iteration.
-                else {
-                    for (var j = 0; j < outputFloatArray.length; j++)
-                        outputFloatArray[j] = 0;
-                }
-                this.script_node_offset = 0;
-            }
-
-            outl[i] = outr[i] = outputFloatArray[this.script_node_offset++];
-        }
-    },
-
-    // immediately play the modulated audio exactly once. Useful for debugging single packets
-    playBuffer: function(obj, func) {
-        console.log("-- playAudioBuffer --");
-        var bufferNode = window.audioContext.createBufferSource();
-        bufferNode.buffer = this.outputAudioBuffer;
-        bufferNode.connect(window.audioContext.destination); // Connect to speakers
-        bufferNode.addEventListener("ended", function() {
-            var playTimeEnd = performance.now();
-            var timeElapsed = playTimeEnd - this.playTimeStart;
-            console.log("got audio ended event after " + timeElapsed.toFixed(2) + "ms");
-            if (obj && func)
-                func.call(obj);
-        }.bind(this));
-        this.playTimeStart = performance.now();
-        bufferNode.start(0); // play immediately
-    },
-
-    // Plays through an entire file. You need to set the callback so once
-    // a single audio packet is finished, the next can start. The index
-    // tells where to start playing. You could, in theory, start modulating
-    // part-way through an audio stream by setting index to a higher number on your
-    // first call.
-    playLoop: function(obj, end_func, param) {
-
-        this.get_more_data = function() {
-            if (!end_func.call(obj, param)) {
-                this.playing = false;
-                return false;
-            }
-            return true;
-        };
-
-        this.script_node.onaudioprocess = function(ev) {
-            Modulator.prototype.processAudio.call(this, ev);
-        }.bind(this);
-
-        if (!this.playing) {
-            this.playing = true;
-            this.script_node.connect(window.audioContext.destination);
-        }
-    },
-
-    modulatePcm: function(data, type) {
 
         var bufLen = Math.ceil(data.length * 8 * this.encoder.samplesPerBit());
         var modulatedData = new Float32Array(bufLen);
